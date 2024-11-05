c3c3be800e2c915418226e273368841562086660
SifatSararChistee <sifatsararchistee@gamil.com>
2024-11-05 15:05:14 +0600

    added sort btn functions

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -4,20 +4,26 @@ import { getAllProducts } from "../Utilities";
 
 const Cart = () => {
     const [products, setProducts]=useState([])
-    const [price , setPrice]= useState(0)
     useEffect(()=>{
         const addedProducts = getAllProducts()
         setProducts(addedProducts)
     },[])
 
     const totalCost = products.reduce((sum, product) => sum + product.price, 0);
+
+    const handleSortBtn=()=>{
+        const sorted = [...products].sort((a,b)=>b.price - a.price)
+        setProducts(sorted)
+    }
+
+
     return (
         <div>
             <div className="flex justify-between mt-5">
                 <p className="text-2xl font-extrabold">Cart</p>
                 <div className="flex gap-4 items-center"> 
                 <p className="text-2xl font-bold">Total Cost: <span className="font-normal">{totalCost}</span> $</p>
-                <button className="btn rounded-full bg-[#9538E2] text-white">Sort by Price</button>
+                <button onClick={handleSortBtn} className="btn rounded-full bg-[#9538E2] text-white">Sort by Price</button>
                 <button className="btn rounded-full bg-[#9538E2] text-white">Purchase</button>
                 </div>
             </div>
