87d21fca137f2d86f40fcc07c1f8916bd7192f90
SifatSararChistee <sifatsararchistee@gamil.com>
2024-11-06 16:03:57 +0600

    added final touch

--- src/Pages/Statistics.jsx
+++ src/Pages/Statistics.jsx
@@ -6,7 +6,7 @@ const Statistics = () => {
     const tittle="Statistics"
     const subTittle="Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!"
     return (
-        <div className="text-xl">
+        <div>
             <Helmet>
         <title>Gadget Heaven Statistics</title>
         <meta name="description" content="View statistics of our store's performance" />
